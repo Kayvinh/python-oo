4b6c5968ad7ec91e4dd901a49a7041961651c5d3
Kevin Nguyen <kayvinhnguyen@gmail.com>
2023-03-07 15:20:03 -0800

    finished serial.py

--- serial.py
+++ serial.py
@@ -17,3 +17,20 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    def __init__(self, start=0):
+        """ Constructor function for SerialGenerator
+            Setting start attribute counter to 0 if start = undefined
+            Setting increment counter to start
+        """
+        self.start = start
+        self.increment = start
+    
+    def generate(self):
+        """ Returns next generated serial number """
+        self.increment += 1
+        return self.increment - 1
+
+    def reset(self):
+        """ Reset serial number to start """
+        self.increment = self.start
