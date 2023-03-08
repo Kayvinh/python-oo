c6b8bc46fee9ebbaca7aae76e8e3f728ed8d2911
Kevin Nguyen <kayvinhnguyen@gmail.com>
2023-03-07 17:38:27 -0800

    finished up to further study

--- wordfinder.py
+++ wordfinder.py
@@ -2,43 +2,32 @@ from random import choice
 
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
-    def __init__(self, file):
-        self.file = open(file)
-        self.words = []
-        self.getWords()
+    def __init__(self, path):
+        """ Opens files and fills list with words and prints word count """
+        file = open(path)
+        self.words = self.get_words(file)
+        file.close()
         print (f"{len(self.words)} words read")
 
-    def getWords(self):
-        for line in self.file:
-            #remove "/n" from end of line
-            line = line[:len(line)-1:] #maybe use strip from lecture python-ds
-            self.words.append(line)
-        self.file.close()
+    def get_words(self, file):
+        """ Returns list of words without whitespace including \n """
+        return [line.strip() for line in file]
+
     def random (self):
+        """ Returns random word from words list """
         return choice(self.words)
 
-
+    def __repr__(self):
+        return f"<{self.__class__.__name__}> words={self.words}"
 
 
 class SpecialWordFinder(WordFinder):
-    def __init__ (self, file):
-        super().__init__(file)
-
-    def getWords(self):
+    """ SpecialWordFinder: filters invalid words from a list """
 
 
 
-
-
-
-
-# TRIED AND DID NOT WORK
-# def getWords(self):
-#         super().getWords()
-#         for word in self.words:
-#             if len(word) != 0 and not (word.startswith("#", 0)):
-#                 self.not_special_words.append(word)
-
-
-#         print(self.not_special_words)
+    def get_words(self, file):
+        """ Returns list of words without comments or whitespace words """
+        return [word for word in super().get_words(file)
+            if word != "" and not word.startswith("#", 0)]
 
